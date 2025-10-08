e4120e3e4d0c2390d68a40388fd7dd926bf970e0
vexarch <vexarch829@gmail.com>
2025-10-08 19:37:23 +0100

    updated example to handle controllers destractors properly

--- example/main.cpp
+++ example/main.cpp
@@ -2,25 +2,26 @@
 #include <nlohmann/json.hpp>
 #include <string>
 #include <vector>
+#include <filesystem>
+#include <csignal>
 #include "server.h"
 #include "controller.h"
 #include "http.hpp"
 #include "vx_database.hpp"
 
-struct Product {
-    int id;
-    std::string name;
-    float price;
-};
-
 const std::string HOST = "127.0.0.1";
 const uint16_t PORT = 8080;
 
-std::vector<Controller*> controllers;
-
 class ProductsController: public Controller {
 private:
+    struct Product {
+        int id;
+        std::string name;
+        float price;
+    };
+
     TypedTable<Product> table;
+
     http::response Get(http::request &req) override {
         if (req.uri.route.size() != 1)
             return http::not_found();
@@ -42,15 +43,29 @@ public:
         table.add_element({5, "Corsair Vengeance DDR5 32GB", 159.99});
         table.add_element({6, "G.Skill Trident Z5 RGB 32GB", 169.99});
     }
-    ~ProductsController() {}
+    ~ProductsController() {
+        std::cout << "Hello" << std::endl;
+        table.clear();
+        fs::remove("products_table.db");
+        fs::remove("products_table_strings.db");
+    }
 };
 
+Server* s = nullptr;
+
+// use the signal handler if you wanted the destractors of controllers to be called when terminating
+void signal_handler(int sig) {
+    std::cout << std::endl;
+    if (s != nullptr) s->terminate();
+}
+
 int main(int argc, char** argv) {
-    controllers = {new ProductsController};
+    std::signal(SIGINT, signal_handler);
     Server server(HOST, PORT);
     server.use_static_files("example/wwwroot");
-    server.use_controllers(controllers);
+    server.use_controllers<ProductsController>();
     std::cout << "Server started on http://" << HOST << ":" << PORT << " ..." << std::endl;
+    s = &server;
     server.listen_for_clients(10);
 
     return 0;
